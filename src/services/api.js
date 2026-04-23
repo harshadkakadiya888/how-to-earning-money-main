2c08ce3d0694e71d3cfe2fd6d4b2c6022b7bfe90
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-04-23 23:41:31 +0530

    add cloudinary packag

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { getToken } from "../utils/auth";
 
-const API = import.meta.env.VITE_API_URL;
+const API = (import.meta.env.VITE_API_URL || "http://127.0.0.1:8000").replace(/\/$/, "");
 
 const api = axios.create({
   baseURL: API,
   headers: {
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true",
   },
 });
 
@@ -20,6 +19,20 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (!error?.response) {
+      console.error("Network/CORS error (no response):", {
+        message: error?.message,
+        url: error?.config?.baseURL ? `${error.config.baseURL}${error.config.url || ""}` : error?.config?.url,
+        method: error?.config?.method,
+      });
+    }
+    return Promise.reject(error);
+  },
 );
 
 export default api;
